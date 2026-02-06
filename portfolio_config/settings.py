6cf45965c2db6617e4ca30a70e5ac736c871af96
pope <johnfaii285@gmail.com>
2026-02-06 10:36:09 +0300

    Fix ALLOWED_HOSTS for Render

--- portfolio_config/settings.py
+++ portfolio_config/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
+ALLOWED_HOSTS = env.list(
+    "ALLOWED_HOSTS", default=[".onrender.com", "localhost", "127.0.0.1"]
+)
 
 # Application definition
 INSTALLED_APPS = [
